80ee18a0a7d7b7a3d4df650a08236e8cea4c3506
Alexander Jair <alexjair@gmail.com>
2023-05-12 01:24:53 -0500

    Subir ejemplos C#

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp;
+
+clsAlumno objEstudiante = new clsAlumno("Juan",18,18.41,"Sistemas");
+objEstudiante.nombre = "July";
+objEstudiante.edad = 36;
+objEstudiante.nota = 18.50;
+objEstudiante.grado = "Secundaria";
+
+clsAlumno objCachimbo = new clsAlumno("Laura", 19, 13.41, "Contabilidad");
+objCachimbo.nombre = "Jair";
+objCachimbo.edad = 23;
+objCachimbo.nota = 15.50;
+objCachimbo.grado = "Primaria";
+
+Console.WriteLine(objEstudiante.nombre);
+Console.WriteLine(objEstudiante.edad);
+Console.WriteLine(objEstudiante.nota);
+Console.WriteLine(objEstudiante.grado);
+
+Console.WriteLine(objCachimbo.nombre);
+Console.WriteLine(objCachimbo.edad);
+Console.WriteLine(objCachimbo.nota);
+Console.WriteLine(objCachimbo.grado);
---
+++ clsAlumno.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class clsAlumno
+    {
+        //atributos
+        public string nombre;
+        public int edad;
+        public double nota;
+        public string grado;
+
+        //constructor
+        public clsAlumno(
+            string vNombre,
+            int vEdad,
+            double vNota,
+            string vGrado
+        ){
+            nombre = vNombre;
+            edad = vEdad;
+            nota = vNota;
+            grado = vGrado;
+        }
+    }
+}
